68f9ddf162b73269883b19474406af06c1d2fb7e
brul1ka <yr.bunik@gmail.com>
2026-02-05 23:40:16 +0200

    add .gitignore and implement task loading and saving to json

--- commands.go
+++ commands.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
+
+var taskID int
+var tasks []Task
+
+func loadTasks() {
+	data, err := os.ReadFile("tasks.json")
+	if err != nil {
+		fmt.Println("INFO:", err)
+		return
+	}
+	json.Unmarshal(data, &tasks)
+	if len(tasks) > 0 {
+		taskID = tasks[len(tasks)-1].ID + 1
+	}
+
+}
 
 func checkForCommands(command string) {
 	switch command {
@@ -13,7 +30,17 @@ func checkForCommands(command string) {
 			break
 		}
 		text := os.Args[2]
-		fmt.Println("Adding task:", text)
+		fmt.Printf("Adding task \"%s\"...\n", text)
+
+		loadTasks()
+		t := Task{ID: taskID, Text: text, Done: false}
+		tasks = append(tasks, t)
+
+		data, _ := json.MarshalIndent(tasks, "", "\t")
+		os.WriteFile("tasks.json", data, 0644)
+
+		fmt.Println("Added!")
+
 	default:
 		printErrorMessage("Command \"" + os.Args[1] + "\" not found")
 	}
