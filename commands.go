a0e29bff4bc6ab92be2ae1b3a0912b81e3331ee4
brul1ka <yr.bunik@gmail.com>
2026-02-06 21:44:45 +0200

    implement task deletion and completion features; enhance loadTasks function

--- commands.go
+++ commands.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 )
 
 var taskID int
@@ -12,7 +14,9 @@ var tasks []Task
 func loadTasks() {
 	data, err := os.ReadFile("tasks.json")
 	if err != nil {
-		fmt.Println("INFO:", err)
+		fmt.Println("INFO: no tasks.json file, creating...")
+		os.WriteFile("tasks.json", []byte("[]"), 0644)
+		fmt.Println("Created!")
 		return
 	}
 	json.Unmarshal(data, &tasks)
@@ -23,6 +27,7 @@ func loadTasks() {
 }
 
 func checkForCommands(command string) {
+	loadTasks()
 	switch command {
 	case "add":
 		if len(os.Args) < 3 {
@@ -32,7 +37,6 @@ func checkForCommands(command string) {
 		text := os.Args[2]
 		fmt.Printf("Adding task \"%s\"...\n", text)
 
-		loadTasks()
 		t := Task{ID: taskID, Text: text, Done: false}
 		tasks = append(tasks, t)
 
@@ -40,6 +44,54 @@ func checkForCommands(command string) {
 		os.WriteFile("tasks.json", data, 0644)
 
 		fmt.Println("Added!")
+	case "list":
+		if len(tasks) <= 0 {
+			fmt.Println("No tasks in file, nothing to show!")
+		} else {
+			for i := range tasks {
+				bold.Printf("%d. \"%s\". Done: %v\n", tasks[i].ID, tasks[i].Text, tasks[i].Done)
+			}
+		}
+	case "delete":
+		if len(os.Args) < 3 {
+			printErrorMessage("Task ID required after 'delete'")
+			break
+		}
+
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			printErrorMessage("Invalid Task ID (must be a number)")
+			break
+		}
+
+		if i := slices.IndexFunc(tasks, func(t Task) bool { return t.ID == id }); i != -1 {
+			tasks = slices.Delete(tasks, i, i+1)
+			data, _ := json.MarshalIndent(tasks, "", "\t")
+			os.WriteFile("tasks.json", data, 0644)
+			fmt.Println("Deleted!")
+		} else {
+			printErrorMessage("Task not found")
+		}
+	case "done":
+		if len(os.Args) < 3 {
+			printErrorMessage("Task ID required after 'done'")
+			break
+		}
+
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			printErrorMessage("Invalid Task ID (must be a number)")
+			break
+		}
+
+		if i := slices.IndexFunc(tasks, func(t Task) bool { return t.ID == id }); i != -1 {
+			tasks[i].Done = true
+			data, _ := json.MarshalIndent(tasks, "", "\t")
+			os.WriteFile("tasks.json", data, 0644)
+			fmt.Printf("Task %d marked as done!\n", id)
+		} else {
+			printErrorMessage("Task not found")
+		}
 
 	default:
 		printErrorMessage("Command \"" + os.Args[1] + "\" not found")
